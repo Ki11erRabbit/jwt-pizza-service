59422c2704d19a0dff65512202328b4edfb7d8ca
ki11errabbit <ki11errabbit@posteo.net>
2024-09-30 12:08:49 -0600

    adding tests back to lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,5 @@ export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.node }},
   {languageOptions: { globals: globals.jest }},
-  pluginJs.configs.recommended,
-  { ignores: ["config.js"] },
-  { ignores: ["*.test.js"] }
+  pluginJs.configs.recommended
 ];
